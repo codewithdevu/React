482c0ec131a8e84a823039cb5d6dfeaa5f2a0393
codewithdevu <divyanshyadav8302@gmail.com>
2026-01-16 00:53:40 +0530

    add the file

--- createreact/createreact.js
+++ createreact/createreact.js
@@ -1,8 +1,17 @@
 function customrender(reactElement , container){
-    const domElement = document.createElement(reactElement.type)
-    domElement.innerHTMl = reactElement.Children
-    domElement.setAttribute('href' , reactElement.props.href)
-    domElement.setAttribute('target' , reactElement.props.target)
+    // const domElement = document.createElement(reactElement.type)
+    // domElement.innerHTML = reactElement.Children
+    // domElement.setAttribute('href' , reactElement.props.href)
+    // domElement.setAttribute('target' , reactElement.props.target)
+
+    // container.appendChild(domElement)
+
+    const domElement = document.createElement(reactElement.type);
+    domElement.innerHTML = reactElement.Children;
+    for (const prop in reactElement.props) {
+        if(prop === 'children') continue ;
+        domElement.setAttribute(prop , reactElement.props[prop]);        
+    }
 
     container.appendChild(domElement)
 }
