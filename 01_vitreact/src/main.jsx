074639626312859f547d47e5a2669859387bb477
codewithdevu <divyanshyadav8302@gmail.com>
2026-01-16 13:17:28 +0530

    add the file

--- 01_vitreact/src/main.jsx
+++ 01_vitreact/src/main.jsx
@@ -1,9 +1,35 @@
-import { StrictMode } from 'react'
+import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import App from './App.jsx' 
+
+function Myapp () {
+  return(
+    <div>
+      <h1>devu in react !</h1>
+    </div>
+  )
+}
+
+// const reactElement = {
+//     type: 'a',
+//     props: {
+//         href: 'https://google.com',
+//         target: '_blank'
+//     },
+//     Children: 'click me to visit google '
+// }
+
+const anotherElement = (
+  <a href="https://google.com" target='_blank'>visit google</a>
+)
+
+const reactElement = React.createElement(
+  'a',
+  {href: 'https://google.com', target: '_blank'},
+  'visit my google'
+)
+
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    reactElement
 )
