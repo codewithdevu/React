6cd534b88cb7c542ff83d119bb0ec25efd4f5e7a
codewithdevu <divyanshyadav8302@gmail.com>
2026-01-18 00:48:42 +0530

    add the last change

--- 04_bg_remover/src/App.jsx
+++ 04_bg_remover/src/App.jsx
@@ -1,14 +1,29 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import './index.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [color, setColor] = useState("rgb(68, 67, 67")
 
   return (
-    <>
-    </>
+    <div className='w-full h-screen duration-200' style={{ backgroundColor: color }}>
+
+      <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>
+
+        <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-3 rounded-full text-black'>
+          <button onClick={() => setColor("red")}
+            className='outline-none px-4 py-1 rounded-full shadow-lg text-white' style={{ backgroundColor: "red" }}> Red</button>
+          <button onClick={() => setColor("green")}
+            className="outline-none px-4 py-1 rounded-full shadow-lg text-white" style={{ backgroundColor: "green" }}> green</button>
+          <button onClick={() => setColor("blue")}
+            className="outline-none px-4 py-1 rounded-full shadow-lg text-white" style={{ backgroundColor: "blue" }}> blue</button>
+        </div>
+
+      </div>
+
+    </div>
+
   )
 }
 
