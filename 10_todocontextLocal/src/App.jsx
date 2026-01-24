879d666313223d728a5450300aad31a58f678fe2
codewithdevu <divyanshyadav8302@gmail.com>
2026-01-24 22:27:51 +0530

    add the context

--- 10_todocontextLocal/src/App.jsx
+++ 10_todocontextLocal/src/App.jsx
@@ -2,14 +2,29 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { TodoProvider } from './Context'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todos , settodos ] = useState([])
+
+  const addTodo = (newtodo) => {
+    settodos((oldtodo) => [{id: Date.now() , ...newtodo }, ...oldtodo])
+  }
 
   return (
-    <>
-      <h1>i become rich one day </h1>
-    </>
+    <TodoProvider value={{todos , toggleComplete , addTodo , deleteTodo , updateTodo}}>
+      <div className="bg-[#172842] min-h-screen py-8">
+        <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
+          <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
+          <div className="mb-4">
+            {/* Todo form goes here */}
+          </div>
+          <div className="flex flex-wrap gap-y-3">
+            {/*Loop and Add TodoItem here */}
+          </div>
+        </div>
+      </div>
+    </TodoProvider>
   )
 }
 
