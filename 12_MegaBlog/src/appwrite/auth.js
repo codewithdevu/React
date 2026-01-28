96beb1856ff297a1b4153c898a1a86175f5edb1a
codewithdevu <divyanshyadav8302@gmail.com>
2026-01-28 23:17:25 +0530

    Add the codes

--- 12_MegaBlog/src/appwrite/auth.js
+++ 12_MegaBlog/src/appwrite/auth.js
@@ -12,6 +12,38 @@ constructor() {
         .setProject(config.appwriteProjectId)
     this.account = new Account(this.client)
 }
+
+async createAccount({email , password , name}){
+    try {
+        const userAccount = await this.account.create( ID.unique(), email , password , name);
+
+        if (userAccount) {
+            // call another method
+            return this.login({email , password});
+        } else {
+            return userAccount;
+        }
+    } catch (error) {
+        throw error;        
+    }
+}
+
+async login ({email , password}) {
+    try {
+        return await this.account.createEmailPasswordSession(email , password);
+    } catch (error) {
+        throw error;
+    }
+}
+
+async getCurrentUser() {
+    try {
+        return await this.account.get()
+    } catch (error) {
+        console.log("Appwrite service :: getCurrentuser :: error" , error);
+    }
+    return null;
+}
 }
 
 const authservice = new Authservice()
