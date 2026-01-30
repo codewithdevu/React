1d6687ccb48abba99c42f40ab563ee411d8aa3b9
codewithdevu <divyanshyadav8302@gmail.com>
2026-01-30 22:31:13 +0530

    Add the files

--- 12_MegaBlog/src/components/Header/Header.jsx
+++ 12_MegaBlog/src/components/Header/Header.jsx
@@ -1,6 +1,42 @@
 import React from 'react'
+import {container , Logo , LogoutBtn} from '../index'
+import { Link } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import { act } from 'react'
 
 function Header() {
+    const authstatus = useSelector((state) => 
+        state.auth.status)
+    const navigate = useNavigate()
+
+    const navItems = [
+        {
+            name: 'Home',
+            slug: '/',
+            active: true
+        },
+        {
+            name: "Login",
+            slug: "/login",
+            active: !authstatus,
+        },
+        {
+            name: "Signup",
+            slug: "/signup",
+            active: !authstatus,
+        },
+        {
+            name:"All Posts",
+            slug: "/add-posts",
+            acitve: authstatus,
+        },
+        {
+            name: "Add post",
+            slug: "/add-post",
+            active: authstatus,
+        },
+    ]
     return (
         <h1>Header</h1>
     )
--- 12_MegaBlog/src/components/index.js
+++ 12_MegaBlog/src/components/index.js
@@ -1,4 +1,7 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
+import container from './container/container'
+import Logo from "./logo";
+import LogoutBtn from "./Header/logoutBtn";
 
-export {Header , Footer};
+export {Header , Footer , container , Logo , LogoutBtn};
