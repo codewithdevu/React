08a17355ff66440b17d5a016ebb3fae9b77e993c
codewithdevu <divyanshyadav8302@gmail.com>
2026-02-04 22:41:37 +0530

    fucking debug shit

--- 12_MegaBlog/src/components/Signup.jsx
+++ 12_MegaBlog/src/components/Signup.jsx
@@ -1,91 +1,89 @@
 import React, { useState } from 'react'
-import authService from '../appwrite/auth'
-import { Link, useNavigate } from 'react-router-dom'
-import { login } from '../store/authSlice'
-import { Button, Input, Logo } from './index.js'
 import { useDispatch } from 'react-redux'
+import { Link, useNavigate } from 'react-router-dom'
+import authservice from '../appwrite/auth'
+import { login } from '../store/authSlice'
+import { Button, Input, Logo } from './index'
 import { useForm } from 'react-hook-form'
 
 function Signup() {
     const navigate = useNavigate()
-    const [error, setError] = useState("")
+    const [error, seterror] = useState("")
     const dispatch = useDispatch()
     const { register, handleSubmit } = useForm()
 
-    const create = async (data) => {
-        setError("")
+    const createSignup = async (data) => {
+        seterror("")
         try {
-            const account = await authService.createAccount(data)
-
-            if (account) {
-                const currentUser = await authService.getCurrentUser()
-                if (currentUser) {
-                    dispatch(login({ userData: currentUser }))
+            const userData = await authservice.createAccount(data)
+            if (userData) {
+                const userData = await authservice.getCurrentUser()
+                if (userData) {
+                    dispatch(login(userData));
+                    navigate('/')
                 }
-                navigate("/")
             }
         } catch (error) {
-            setError(error?.message || "Signup failed")
+            seterror(error.message)
         }
     }
-
-
     return (
-        <div className="flex items-center justify-center">
-            <div className={`mx-auto w-full max-w-lg bg-gray-100 rounded-xl p-10 border border-black/10`}>
-                <div className="mb-2 flex justify-center">
-                    <span className="inline-block w-full max-w-100px">
-                        <Logo width="100%" />
+        <div className=' fles items-center justify-center '>
+            <div className={`mx-auto w-full max-w-lg bg-gray-100 rounded-b-xl p-10 border border-black/10`}>
+                <div className='mb-2 flex justify-center'>
+                    <span className='inline-block w-full max-w-100px'>
+                        <Logo width='100%' />
                     </span>
                 </div>
-                <h2 className="text-center text-2xl font-bold leading-tight">Sign up to create account</h2>
-                <p className="mt-2 text-center text-base text-black/60">
-                    Already have an account?&nbsp;
+                <h2 className='text-center text-2xl font-bold leading-tight'>Sign up to create account</h2>
+                <p className='mt-2 text-center text-base text-black/60'>Already have an account?&nbsp;
                     <Link
-                        to="/login"
-                        className="font-medium text-primary transition-all duration-200 hover:underline"
+                        to='/login'
+                        className='font-medium text-primary transition-all duration-200 hover:underline'
                     >
-                        Sign In
+                        Sign up
                     </Link>
                 </p>
-                {error && <p className="text-red-600 mt-8 text-center">{error}</p>}
-
-                <form onSubmit={handleSubmit(create)}>
+                {error && <p className='text-red-600 mt-8 text-center'>{error}</p>}
+                <form
+                    onSubmit={handleSubmit(createSignup)}
+                    className='mt-8'>
                     <div className='space-y-5'>
                         <Input
-                            label="Full Name: "
+                            label='Full name: '
+                            type='full name'
                             placeholder="Enter your full name"
-                            {...register("name", {
-                                required: true,
-                            })}
+                        {...register('name', {
+                            required: true,
+                        })}
                         />
                         <Input
-                            label="Email: "
+                            label='Email: '
                             placeholder="Enter your email"
                             type="email"
-                            {...register("email", {
+                            {...register('email', {
                                 required: true,
                                 validate: {
-                                    matchPatern: (value) => /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(value) ||
-                                        "Email address must be a valid address",
+                                    matchpattern: (value) => /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/.test(value) || "Email addres must be a valid address",
                                 }
                             })}
                         />
                         <Input
-                            label="Password: "
-                            type="password"
-                            placeholder="Enter your password"
-                            {...register("password", {
+                            label='Password: '
+                            type='Password'
+                            placeholder="Enter you password"
+                            {...register('password', {
                                 required: true,
-                            })}
+                                validate: {
+                                    matchpattern: (pass) => /(?=^.{8,}$)(?=.*\d)(?=.*\W+)(?=.*[A-Z])(?=.*[a-z])(?!.*\n).*$/.test(pass) || "Password invalid"
+                                }
+
+                            })} 
                         />
-                        <Button type="submit" className="w-full">
-                            Create Account
-                        </Button>
+                        <Button type='submit' className='w-full'>Create account</Button>
                     </div>
                 </form>
             </div>
-
         </div>
     )
 }
--- 12_MegaBlog/src/main.jsx
+++ 12_MegaBlog/src/main.jsx
@@ -1,78 +1,86 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import './index.css'
 import { Provider } from 'react-redux'
 import store from './store/store.js'
-import { BrowserRouter, createBrowserRouter } from 'react-router-dom'
-import { AuthLayout, Login, } from './components/index.js'
-import { Addpost, Allposts, Signup, EditPost, Post } from './pages/index.js'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Home from './pages/Home.jsx'
+import { AuthLayout, Login } from './components/index.js'
 
 
+import AddPost from "./pages/AddPost";
+import Signup from './pages/Signup'
+import EditPost from "./pages/EditPost";
+
+import Post from "./pages/Post";
+
+import AllPosts from "./pages/AllPosts";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
     children: [
-      {
-        path: '/',
-        element: <home />,
-      },
-      {
-        path: "login",
-        element: (
-          <AuthLayout authenticaton={false}>
-            <Login />
-          </AuthLayout>
-        ),
-      },
-      {
-        path: "/signup",
-        element: (
-          <AuthLayout authenticaton={false}>
-            <Signup />
-          </AuthLayout>
-        ),
-      },
-      {
-        path: "/all-posts",
-        element: (
-          <AuthLayout authenticaton={" "}>
-            <Allposts />
-          </AuthLayout>
-        ),
-      },
-      {
-        path: "/add-post",
-        element: (
-          <AuthLayout authenticaton={" "}>
-            <Addpost />
-          </AuthLayout>
-        ),
-      },
-      {
-        path: "/edit-post/:slug",
-        element: (
-          <AuthLayout authenticaton={" "}>
-            <EditPost />
-          </AuthLayout>
-        ),
-      },
-      {
-        path: "/post/:slug",
-        element: <Post />
-      }
-
-    ]
-  }
+        {
+            path: "/",
+            element: <Home />,
+        },
+        {
+            path: "/login",
+            element: (
+                <AuthLayout authentication={false}>
+                    <Login />
+                </AuthLayout>
+            ),
+        },
+        {
+            path: "/signup",
+            element: (
+                <AuthLayout authentication={false}>
+                    <Signup />
+                </AuthLayout>
+            ),
+        },
+        {
+            path: "/all-posts",
+            element: (
+                <AuthLayout authentication>
+                    {" "}
+                    <AllPosts />
+                </AuthLayout>
+            ),
+        },
+        {
+            path: "/add-post",
+            element: (
+                <AuthLayout authentication>
+                    {" "}
+                    <AddPost />
+                </AuthLayout>
+            ),
+        },
+        {
+            path: "/edit-post/:slug",
+            element: (
+                <AuthLayout authentication>
+                    {" "}
+                    <EditPost />
+                </AuthLayout>
+            ),
+        },
+        {
+            path: "/post/:slug",
+            element: <Post />,
+        },
+    ],
+},
 ])
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+    <RouterProvider router={router}/>
     </Provider>
-  </StrictMode>,
+  </React.StrictMode>,
 )
